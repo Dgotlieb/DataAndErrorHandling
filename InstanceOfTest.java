204118e5b6b182f5ee3d1b82baecf1dbf7e2b0cb
dgotlieb <Dgotlieb8@gmail.com>
2021-10-09 13:45:51 +0300

    Delete InstanceOfTest.java

--- InstanceOfTest.java
+++
@@ -1,9 +0,0 @@
-public class InstanceOfTest {
-    public static void main(String[] args) {
-        Cow a = new Cow();
-
-        System.out.println(a instanceof Cow); // true
-        System.out.println(a instanceof Animal); // true
-        System.out.println(a instanceof Inter); //true
-    }
-}
